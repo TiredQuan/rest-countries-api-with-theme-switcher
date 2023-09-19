6e495347904039a9492a813abea7811f5ff02600
Nguyễn Minh Quân <85377764+TiredQuan@users.noreply.github.com>
2023-09-19 11:25:35 +0700

    add margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Header from "./components/Header"
 
 export default function App() {
 
   return (
-    <>
-
-    </>
+    <div className="px-40">
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <>
+        <header>
+            <nav className='flex justify-between'>
+                <h1>Where in the world</h1>
+                <button>Dark Mode</button>
+            </nav>
+        </header>
+        </>
+    )
+}
